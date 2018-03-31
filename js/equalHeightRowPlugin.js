b1e96d99dedb63cbcd416843d8c51bedca4ace66
Rowan Jordaan <rowanjordaan@gmail.com>
2018-03-31 17:59:20 +0200

    Added row wrapping and items dom output in return

--- js/equalHeightRowPlugin.js
+++ js/equalHeightRowPlugin.js
@@ -7,32 +7,36 @@
             onResize: null, // Called on resize (expects function)
             itemSelector : '.item', // Class of item within the container selector,
             innerSelectors: null, // Comma seperated element selector for equal height within the items
+            wrapRows: false // Classname to wrap rows in
+
         }, options);
 
         var _this = this;
         var _$this = $(_this);
 
-        var output = {};
+        var output = {}; // Return object for plugin
 
         /*
         *   Equal Height
         */
-        var _setEqualHeight = function($element){
+        var _setEqualHeight = function($items){
             var heighest = 0; // Counter
 
             // Reset height so we can calculate from base heigh
-            $element.css({'height' : 'auto'});
+            $items.css({'height' : 'auto'});
 
-            // Go through each item of current row and remember the heighest item
-            $element.each(function(){
-                var itemHeight = $(this).outerHeight();
+            // Go through each item of current row and remember the heighest item if there's more than one item for peformance
+            if($items.length > 1){
+                $items.each(function(){
+                    var itemHeight = $(this).outerHeight();
 
-                heighest = (itemHeight >= heighest) ? itemHeight : heighest;
-            });
+                    heighest = (itemHeight >= heighest) ? itemHeight : heighest;
+                });
 
-            // Set height of current row to the heighest found item
-            $element.css({'height' : heighest + 'px'});
-        }
+                // Set height of current row to the heighest found item
+                $items.css({'height' : heighest + 'px'});
+            }
+        };
 
         /*
         *   Run calculate rows and set equalHeight
@@ -44,6 +48,7 @@
             rowData.items = rowData.selector.find(settings.itemSelector);
             rowData.itemAmount = rowData.items.length; // Amount of items
             rowData.rowsData = {}; // Save rows information
+            rowData.rowsItems = {}; // Save doms
 
             // Counters
             var previousRow = 0; // Remember previous row
@@ -51,14 +56,18 @@
             var itemWidthCount = 0; // Item width counter untill reached row width
             var containerWidth = rowData.selector.width();
 
+            // Remove the rows wrap so we can calculate new rows
+            if(settings.wrapRows){
+                $(rowData.selector).find('[data-type="row"]').contents().unwrap();
+            }
+
             /*
             *   Calculate rows
             */
             rowData.items.each(function(i, e){
 
                 // Save last item in the row (gets overwritten each item untill reached last)
-                rowData.rowsData['row' + currentRow] = {};
-                rowData.rowsData['row' + currentRow].lastItem = (i+1);
+                rowData.rowsData['row' + currentRow] = { lastItem : (i + 1) };
 
                 // Check if the next item is on a different row
                 var checkNextRow = function(){
@@ -73,11 +82,12 @@
 
                 if(checkNextRow()){
                     // Calculate first row item
-
                     var rowItemDifference = (previousRow == 0) ? rowData.rowsData['row' + currentRow].lastItem : (rowData.rowsData['row' + currentRow].lastItem - rowData.rowsData['row' + previousRow].lastItem);
-
                     var firstRowItem = ((rowData.rowsData['row' + currentRow].lastItem - rowItemDifference) + 1);
                     rowData.rowsData['row' + currentRow].firstItem = firstRowItem; // Set first row item for current row
+
+                    // Find items belonging to current row and save doms as current row
+                    rowData.rowsItems['row' + currentRow] = rowData.items.filter(settings.itemSelector + ':nth-child(n+'+ firstRowItem +'):nth-child(-n+'+ rowData.rowsData['row' + currentRow].lastItem +')'); // Set first row item for current row
 
                     previousRow = currentRow; // Set previous row
                     currentRow ++; // Increment row
@@ -88,12 +98,14 @@
             /*
             *   Calculate & set height for each required element
             */
-            $.each(rowData.rowsData, function(n, e){
 
-                // Find items for current row
-                var $rowItems = rowData.items.filter(settings.itemSelector + ':nth-child(n+'+ e.firstItem +'):nth-child(-n+'+ e.lastItem +')');
+            $.each(rowData.rowsItems, function(i, items){
+                _setEqualHeight(items); // Equal height for base row items
 
-                _setEqualHeight($rowItems); // Equal height for base row items
+                // Wrap items in rows
+                if(settings.wrapRows){
+                    items.wrapAll('<div class="'+ settings.wrapRows +' '+ i + '" data-type="row"></div>')
+                }
 
                 // Optional inner selectors
                 if(settings.innerSelectors != null){
@@ -101,8 +113,8 @@
 
                     // Run equalHeight for every inner selector
                     $.each(innerSelectors, function(int, selector){
-                        _setEqualHeight($rowItems.find(selector));
-                        _setEqualHeight($rowItems);
+                        _setEqualHeight(items.find(selector));
+                        _setEqualHeight(items);
                     });
                 }
             });
@@ -110,24 +122,21 @@
             return rowData;
         };
 
-
         /*
         *   Outside callable function: execute()
         *   Parameters:
         *   iteration = which iteration to run
         */
-        output.execute = function(iteration, selector){
+        output.execute = function(selector){
             var data = {};
 
-            // Check if parameter is given
-            iteration = (iteration !== undefined) ? iteration : false;
             selector = (selector !== undefined) ? selector : false;
 
-            // Re execute for every instance
+            // Run _execute() for every element seperate
             _$this.each(function(i, e){
 
                 // Check if execute needs to run for this instance based on the number given
-                if( ( iteration === false && selector === false) || ( iteration === i && (selector === false || $(e).is(selector) ) ) || ( selector !== false && $(e).is(selector) && ( iteration === false || iteration === i) ) ){
+                if( selector === false || ( selector !== false && $(e).is(selector) ) ){
                     data[i] = _execute($(e));
                 }
             });
